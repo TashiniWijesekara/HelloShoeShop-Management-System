d18c27e80fa31b91a99791d25aea084fa31357ab
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-05-18 10:26:28 +0530

    Initial Commit

---
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/HelloShoeShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.HelloShoeShopManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoeShopManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShoeShopManagementSystemApplication.class, args);
+	}
+
+}
---
+++ HelloShoeShopManagementSystem/src/test/java/lk/ijse/gdse/HelloShoeShopManagementSystem/HelloShoeShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.HelloShoeShopManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoeShopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
