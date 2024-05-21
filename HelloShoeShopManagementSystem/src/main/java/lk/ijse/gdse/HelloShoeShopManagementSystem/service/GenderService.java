288333bbbc52a48565c1cbc6cbe37bc61a1ef8a0
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-05-21 21:15:47 +0530

    update

--- HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/GenderService.java
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/GenderService.java
@@ -1,4 +1,12 @@
 package lk.ijse.gdse.HelloShoeShopManagementSystem.service;
 
+import lk.ijse.gdse.HelloShoeShopManagementSystem.dto.GenderDTO;
+
+import java.util.List;
+
 public interface GenderService {
+    void saveGender(GenderDTO genderDTO);
+    List<GenderDTO> genderGetAll();
+    void updateGender(String id, GenderDTO genderDTO);
+    void deleteGender(String id);
 }
--- HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/IMPL/GenderServiceImpl.java
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/IMPL/GenderServiceImpl.java
@@ -1,4 +1,40 @@
 package lk.ijse.gdse.HelloShoeShopManagementSystem.service.IMPL;
 
-public class GenderServiceImpl {
+import lk.ijse.gdse.HelloShoeShopManagementSystem.conversion.ConversionData;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.dto.GenderDTO;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.exception.NotFoundException;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.repository.GenderServiceDAO;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.service.GenderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class GenderServiceImpl implements GenderService {
+    final private GenderServiceDAO genderServiceDAO;
+    final private ConversionData conversionData;
+    @Override
+    public void saveGender(GenderDTO genderDTO) {
+        genderServiceDAO.save(conversionData.covertToGenderEntity(genderDTO));
+    }
+
+    @Override
+    public List<GenderDTO> genderGetAll() {
+        return conversionData.covertToGenderDTOList(genderServiceDAO.findAll());
+    }
+
+    @Override
+    public void updateGender(String id, GenderDTO genderDTO) {
+        if(genderServiceDAO.existsById(id)) throw new NotFoundException("Gender not found");
+        genderServiceDAO.save(conversionData.covertToGenderEntity(genderDTO));
+    }
+
+    @Override
+    public void deleteGender(String id) {
+        genderServiceDAO.deleteById(id);
+    }
 }
