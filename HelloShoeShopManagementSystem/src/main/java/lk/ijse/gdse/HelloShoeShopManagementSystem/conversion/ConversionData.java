f0580c54980568b271cb04a75dbe91d60bbf6c84
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-05-21 18:36:46 +0530

    update

---
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/conversion/ConversionData.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.HelloShoeShopManagementSystem.conversion;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class ConversionData {
+
+}
--- HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/IMPL/AuthenticationServiceImpl.java
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/IMPL/AuthenticationServiceImpl.java
@@ -1,4 +1,24 @@
 package lk.ijse.gdse.HelloShoeShopManagementSystem.service.IMPL;
 
-public class AuthenticationServiceImpl {
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.conversion.ConversionData;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.repository.UserServiceDAO;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.service.AuthenticationService;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final UserServiceDAO userServiceDAO;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final ConversionData conversionData;
+
+
 }
