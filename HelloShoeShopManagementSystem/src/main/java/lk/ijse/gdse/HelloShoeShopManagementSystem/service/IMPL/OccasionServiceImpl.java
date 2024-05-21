e9b126e7719e184a062ab4baca469282ca4fa357
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-05-21 22:10:24 +0530

    update

--- HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/IMPL/OccasionServiceImpl.java
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/IMPL/OccasionServiceImpl.java
@@ -1,4 +1,41 @@
 package lk.ijse.gdse.HelloShoeShopManagementSystem.service.IMPL;
 
-public class OccasionServiceImpl {
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.conversion.ConversionData;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.dto.OccasionDTO;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.exception.NotFoundException;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.repository.OccasionServiceDAO;
+import lk.ijse.gdse.HelloShoeShopManagementSystem.service.OccasionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OccasionServiceImpl implements OccasionService {
+    final private OccasionServiceDAO occasionServiceDAO;
+    final private ConversionData conversionData;
+    @Override
+    public void saveOccasion(OccasionDTO occasionDTO) {
+        occasionServiceDAO.save(conversionData.convertToOccasionEntity(occasionDTO));
+    }
+
+    @Override
+    public List<OccasionDTO> getAllOccasion() {
+        return conversionData.convertToOccasionDTOList(occasionServiceDAO.findAll());
+    }
+
+    @Override
+    public void updateOccasion(String id, OccasionDTO occasionDTO) {
+        if(!occasionServiceDAO.existsById(id)){throw new NotFoundException("Occasion Not Found");}
+        occasionServiceDAO.save(conversionData.convertToOccasionEntity(occasionDTO));
+    }
+
+    @Override
+    public void deleteOccasion(String id) {
+        occasionServiceDAO.deleteById(id);
+    }
+
 }
--- HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/OccasionService.java
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/OccasionService.java
@@ -1,4 +1,12 @@
 package lk.ijse.gdse.HelloShoeShopManagementSystem.service;
 
+import lk.ijse.gdse.HelloShoeShopManagementSystem.dto.OccasionDTO;
+
+import java.util.List;
+
 public interface OccasionService {
+    void saveOccasion(OccasionDTO occasionDTO);
+    List<OccasionDTO> getAllOccasion();
+    void updateOccasion(String id, OccasionDTO occasionDTO);
+    void deleteOccasion(String id);
 }
