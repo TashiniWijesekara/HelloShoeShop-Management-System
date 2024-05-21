11c70007c86551fdd9d39b7966b10d15bf8b6228
TashiniWijesekara <wijesekaratashini@gmail.com>
2024-05-21 21:54:46 +0530

    update

--- HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/UserService.java
+++ HelloShoeShopManagementSystem/src/main/java/lk/ijse/gdse/HelloShoeShopManagementSystem/service/UserService.java
@@ -1,4 +1,7 @@
 package lk.ijse.gdse.HelloShoeShopManagementSystem.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 public interface UserService {
+    UserDetailsService userDetailsService();
 }
